af36209a8e83bd0b0c863623a89752de0c3d4bee
FegaMega <david.smidebrant@gmail.com>
2025-12-16 21:18:29 +0100

    fsadfasdf

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function closeModal(modalId) {
       modal.style.display = "none";
       modal.querySelector(".caption").innerText = "";
    }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
